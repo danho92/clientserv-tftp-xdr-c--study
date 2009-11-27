b1ece22c1a6a9a20c6e71b58608e9ba661d45559
luca.pwns <luca.pwns@78c57f12-d90d-11de-8d17-8328ca00869f>
2009-11-27 20:56:04 +0000

    go on with tri-state + stderr switch

--- xdr_udp_utils.h
+++ xdr_udp_utils.h
@@ -11,8 +11,8 @@
 #define MAX_PAYLOAD_SIZE    (8192)
 #define TIMEOUT             (2)
 #define MAX_TRY_COUNT       (16)
-
 #define PORT_ANY            (htons(0))
+
 
 /* gli stream xdr vanno inizializzati (con xdrmem_create) prima dell' *
  * utilizzo delle funzioni che lavorano con XDR                       */
@@ -21,8 +21,11 @@ extern char in_buff[MAX_PAYLOAD_SIZE];
 extern XDR out_xdrs;
 extern char out_buff[MAX_PAYLOAD_SIZE];
 
+/* TRUE se stderr e' disponibile */
+extern bool_t USE_STDERR;
 
 
+/* tipo del valore di ritorno della funzione read_msg() */
 typedef enum read_msg_ret {
   XDR_OK,
   XDR_FAIL,
@@ -39,15 +42,15 @@ void write_msg(int fd, msg_t* msg);
 void sendto_msg(int fd, struct sockaddr_in* srv_addr, msg_t* msg);
 
 /* routine di error reporting */
-void err_rep(FILE* local, int remote_fd, errcode_t errcode, char* errstr);
+void err_rep(int fd, errcode_t errcode, char* errstr);
 
 /* serializzazione e invio per messaggi ERR ed ACK */
 void write_ERR(int fd, errcode_t errcode, char* errstr);
 void write_ACK(int fd, blockn_t blocknum);
 
 /* routine di invio/ricezione file utili sia al server che al client */
-void get_file(int in, int out, FILE* ferr, FILE* fout, bool_t ack0);
-void put_file(int in, int out, FILE* ferr, FILE* fin, bool_t ack0);
+void get_file(int in, int out, FILE* fout, bool_t ack0);
+void put_file(int in, int out, FILE* fin, bool_t ack0);
 
 
 #endif /* _XDR_UDP_UTILS_H_ */
