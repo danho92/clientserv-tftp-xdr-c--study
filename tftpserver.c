6f948642547198977c0fb8c733a34f3c360d2e6e
luca.pwns <luca.pwns@78c57f12-d90d-11de-8d17-8328ca00869f>
2009-12-01 17:05:20 +0000

    tftpserver.c seems ok

--- tftpserver.c
+++ tftpserver.c
@@ -23,6 +23,7 @@ char in_buff[MAX_RAW_MSG_SIZE];
 XDR out_xdrs;
 char out_buff[MAX_RAW_MSG_SIZE];
 
+/* stderr non disponibile (flag richiesta dal modulo xdr_udp_utils) */
 bool_t USE_STDERR = FALSE;
 
 
@@ -41,12 +42,12 @@ int main(int argc, char* argv[]) {
   /* ricezione del primo messaggio */
   if (recvfrom(0, in_buff, MAX_BLOCK_SIZE, 0,
       (struct sockaddr*)&cli_addr, &cli_addr_len) <= 0) {
-    exit(EXIT_FAILURE); // "initial recvfrom() gone wrong."
+    exit(EXIT_FAILURE);
   }
 
   pid = fork();
   if (pid < 0) {
-    exit(EXIT_FAILURE); // "fork() error."
+    exit(EXIT_FAILURE);
   }
   if (pid == 0) {
     /* CHILD */
@@ -56,34 +57,31 @@ int main(int argc, char* argv[]) {
     /* creazione del socket di comunicazione */
     sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (errno != 0) {
-      exit(EXIT_FAILURE); // "couldn't create com socket."
+      exit(EXIT_FAILURE);
     }
 
-    /* bind e "connessione" del socket */
+    /* bind del com socket ad una porta effimera e "connessione" al client */
     memset(&srv_addr, 0x00, sizeof(srv_addr));
     srv_addr.sin_family       = PF_INET;
     srv_addr.sin_port         = PORT_ANY;
     srv_addr.sin_addr.s_addr  = INADDR_ANY;
-
-    if (bind(sock, (struct sockaddr*)&srv_addr, sizeof(srv_addr)) == -1) {
-      exit(EXIT_FAILURE); // "couldn't bind com socket."
-    }
-    if (connect(sock, (struct sockaddr*)&cli_addr, cli_addr_len) == -1) {
-      exit(EXIT_FAILURE); // "couldn't UDP-connect com socket."
+    if ( (bind(sock, (struct sockaddr*)&srv_addr, sizeof(srv_addr)) == -1) ||
+         (connect(sock, (struct sockaddr*)&cli_addr, cli_addr_len) == -1)) {
+      exit(EXIT_FAILURE);
     }
 
     /* dup() su stdin, stdout, e stderr */
     if ((dup2(sock, 0) != 0) ||
         (dup2(sock, 1) != 1) ||
         (dup2(sock, 2) != 2) ) {
-      exit(EXIT_FAILURE); // "couldn't dup() com socket on standard streams."
+      exit(EXIT_FAILURE);
     }
 
-    /* init degli stream xdr */
+    /* init degli stream XDR */
     xdrmem_create(&in_xdrs,  in_buff,  MAX_RAW_MSG_SIZE, XDR_DECODE);
     xdrmem_create(&out_xdrs, out_buff, MAX_RAW_MSG_SIZE, XDR_ENCODE);
 
-    /* passaggio del controllo al server TFTP */
+    /* passaggio del controllo alla routine di servizio TFTP */
     server_loop();
     exit(EXIT_SUCCESS);
   }
@@ -100,6 +98,7 @@ void server_loop() {
   int try;
   bool_t error;
 
+  /* il primo messaggio e' gia' nel buffer associato allo stream XDR */
   if (decode_msg(&msg) == FALSE) {
     err_rep(1, ILL_OP_TFTP, "TFTP message expected");
     error = TRUE;
@@ -146,7 +145,7 @@ void server_loop() {
       }
     } while ((try > 0) && (try < MAX_TRY_COUNT) && (error == FALSE));
 
-    /* niente errori, ma troppi tentativi */
+    /* nessun errore, ma troppi tentativi */
     if ((error == FALSE) && (try == MAX_TRY_COUNT)) {
       err_rep(1, NOT_DEFINED, "too many timeout. type faster!");
       error = TRUE;
@@ -160,16 +159,14 @@ void server_loop() {
  *  PROCESS WRQ
 **/
 void process_WRQ(const char* filename) {
-  FILE* fout;
-
-  fout = fopen(filename, "wb");
+  FILE* fout = fopen(filename, "wb");
   if (fout == NULL) {
     err_rep(1, ACCESS_VIOLATION, "opening file");
     return;
   }
 
+  /* ack0 = TRUE perche' il client deve sapere che REQ e' stato accettato */
   get_file(0, 1, fout, TRUE);
-
   fclose(fout);
 }
 
@@ -178,16 +175,14 @@ void process_WRQ(const char* filename) {
  *  PROCESS RRQ
 **/
 void process_RRQ(const char* filename) {
-  FILE* fin;
-
-  fin = fopen(filename, "rb");
+  FILE* fin = fopen(filename, "rb");
   if (fin == NULL) {
     err_rep(1, ACCESS_VIOLATION, "opening file");
     return;
   }
 
+  /* ack0 = FALSE perche', lato client, l'ACK #0 e' il messaggio REQ */
   put_file(0, 1, fin, FALSE);
-
   fclose(fin);
 }
 
