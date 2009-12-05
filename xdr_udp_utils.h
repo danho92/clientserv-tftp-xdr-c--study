4763791397435105064e7a37e69d7b51dfaf9e00
luca.pwns <luca.pwns@78c57f12-d90d-11de-8d17-8328ca00869f>
2009-12-05 15:13:00 +0000

    almost done

--- xdr_udp_utils.h
+++ xdr_udp_utils.h
@@ -10,7 +10,7 @@
 
 
 #define MAX_RAW_MSG_SIZE    (8192)
-#define TIMEOUT             (1)
+#define TIMEOUT             (2)
 #define MAX_TRY_COUNT       (16)
 #define PORT_ANY            (htons(0))
 
@@ -52,12 +52,12 @@ u_int encode_msg(msg_t* msg);
 void write_msg(int fd, msg_t* msg);
 void sendto_msg(int sock, struct sockaddr_in* srv_addr, msg_t* msg);
 
-/* routine di error reporting remota (e locale se USE_STDERR = TRUE) */
-void err_rep(int fd, errcode_t errcode, char* errstr);
-
 /* shortcut per encoding e invio di messaggi ERR ed ACK */
 void write_ERR(int fd, errcode_t errcode, char* errstr);
 void write_ACK(int fd, blockn_t blocknum);
+
+/* routine di error reporting remota (e locale se USE_STDERR = TRUE) */
+void err_rep(int fd, errcode_t errcode, char* errstr);
 
 /* routine di invio/ricezione file (implementazione del protocollo TFTP) */
 void get_file(int in, int out, FILE* fout, bool_t ack0);
